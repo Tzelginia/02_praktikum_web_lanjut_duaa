70bbeab7546727d495ea36ed71a943d16c86cee4
tzelginianc <tzelginia78@gmail.com>
2022-02-22 10:41:33 +0700

    Penambahan Controller

---
+++ app/Http/Controllers/AboutController.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class AboutController extends Controller
+{
+    public function about(){
+        return "Tzelginia Noer Cahyani-2041720048";
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -16,5 +16,11 @@ use Illuminate\Support\Facades\Route;
 // Route::get('/', function () {
 //     return view('welcome');
 // });
-use App\Http\Controllers\PageController;
-Route::get('/', [PageController::class, 'index']);
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\AboutController;
+use App\Http\Controllers\ArticleController;
+Route::get('/', [HomeController::class, 'index']);
+
+Route::get('/about', [AboutController::class, 'about']);
+
+Route::get('/articles/{id}', [ArticleController::class, 'articles']);
